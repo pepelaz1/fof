65b52cc1e16b0b2153f789063215a0f8226f79ce
Vladimir Baldetskiy <pepelaz101@gmail.com>
2018-04-09 10:06:45 +0700

    2

--- fof/app/src/main/java/ru/pepelaz/fof/MainActivity.kt
+++ fof/app/src/main/java/ru/pepelaz/fof/MainActivity.kt
@@ -1,12 +1,60 @@
 package ru.pepelaz.fof
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageViewFish.setOnTouchListener(touchListener)
+        imageViewRecords.setOnTouchListener(touchListener)
+        imageViewTackle.setOnTouchListener(touchListener)
+        imageViewBalt.setOnTouchListener(touchListener)
+        imageViewFacts.setOnTouchListener(touchListener)
+        imageViewSeasons.setOnTouchListener(touchListener)
+        imageViewLocations.setOnTouchListener(touchListener)
+        imageViewGlossary.setOnTouchListener(touchListener)
+        imageViewCamera.setOnTouchListener(touchListener)
+        imageViewCooking.setOnTouchListener(touchListener)
+        imageViewTides.setOnTouchListener(touchListener)
+        imageViewWeather.setOnTouchListener(touchListener)
+        imageViewContact.setOnTouchListener(touchListener)
+        imageViewAbout.setOnTouchListener(touchListener)
+        imageViewGetLucky.setOnTouchListener(touchListener)
+        imageViewQuiz.setOnTouchListener(touchListener)
+
+        Log.d("test_test","Destiny: " + resources.getDisplayMetrics().density)
+    }
+
+    object touchListener: View.OnTouchListener {
+        override fun onTouch(v: View, m: MotionEvent): Boolean {
+            // Perform tasks here
+            when(m.action) {
+                MotionEvent.ACTION_DOWN -> (v as ImageView).alpha = 0.5.toFloat()
+                MotionEvent.ACTION_UP -> (v as ImageView).alpha = 1.0.toFloat()
+            }
+            return true
+        }
+    }
+
+    fun buttonQuitClicked(v: View) {
+        finish()
+    }
+    fun buttonGoSiteClicked(v: View) {
+        startActivity(Intent(this, SiteActivity::class.java))
+        finish()
     }
 }
---
+++ fof/app/src/main/java/ru/pepelaz/fof/SiteActivity.kt
@@ -0,0 +1,27 @@
+package ru.pepelaz.fof
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_site.*
+
+class SiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_site)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
+        webView.settings.builtInZoomControls = true;
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                imageViewBackground.visibility = View.GONE
+                webView.visibility = View.VISIBLE
+            }
+        }
+        webView.loadUrl("http://www.fish-o-fax.com/")
+    }
+}
