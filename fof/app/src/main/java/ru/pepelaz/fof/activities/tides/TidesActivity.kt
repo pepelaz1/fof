4b6b60569d4bfaaa3dfa8389287cbb9cea69e8e5
Vladimir Baldetskiy <pepelaz101@gmail.com>
2018-05-18 10:26:54 +0700

    !

--- fof/app/src/main/java/ru/pepelaz/fof/activities/tides/TidesActivity.kt
+++ fof/app/src/main/java/ru/pepelaz/fof/activities/tides/TidesActivity.kt
@@ -3,6 +3,8 @@ package ru.pepelaz.fof.activities.tides
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.location.Address
 import android.location.Geocoder
 import android.net.Uri
@@ -11,8 +13,10 @@ import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import android.support.v4.app.FragmentActivity
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.View
+import com.github.mikephil.charting.charts.Chart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -35,15 +39,20 @@ import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import com.github.mikephil.charting.formatter.IValueFormatter
 import kotlinx.android.synthetic.main.activity_tides.*
+import kotlinx.android.synthetic.main.activity_weather.view.*
 import java.text.SimpleDateFormat
 import java.util.concurrent.TimeUnit
 
 
 class TidesActivity() : AppCompatActivity(), TidesCalendarFragment.ITidesCalendarFragment {
 
+    var selectedDate: Date? = null
+    var lineChart: LineChart? = null
+
     override fun onDateSelected(date: Date) {
         Log.d("test_test", "date selected: " + date)
-        rebuildChart()
+        selectedDate = date
+        loadTides()
     }
 
 
@@ -51,8 +60,8 @@ class TidesActivity() : AppCompatActivity(), TidesCalendarFragment.ITidesCalenda
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tides)
 
+        setupChart()
         loadTides()
-        rebuildChart()
     }
 
     fun onHomeClick(v: View) {
@@ -89,71 +98,100 @@ class TidesActivity() : AppCompatActivity(), TidesCalendarFragment.ITidesCalenda
                 CurrentCoords.latitude,
                 CurrentCoords.longitude,
                 1)
-        if (addresses != null && addresses.size > 0) {
+        if (addresses != null && addresses.size > 0 && textView3 != null) {
             textView3.text = Utils.constructLocationName(addresses[0])
         }
-
 
 
        // textViewLatitudeValue.text = CurrentCoords.latitude.toString()
        // textViewLongitudeValue.text = CurrentCoords.longitude.toString()
 
-        val coords = CurrentCoords.latitude.toString() + "," + CurrentCoords.longitude.toString()
+        if (lineChart != null) {
+            val coords = CurrentCoords.latitude.toString() + "," + CurrentCoords.longitude.toString()
 
-
-//        Communicator.service(this)!!.getWeather("b6add83687f743fb94a150227171510", coords, "yes", 24)
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe (
-//                        {data ->
-//                            Log.d("test_test", "data: " + data.weather!![0].date)
-//                            WeatherStorage.set(data.weather!!)
-//                            updateWeatherList()
-//                        },
-//                        {error ->
-//                            Log.d("test_test","Failed to get weather data, error: " + error.toString())
-//                            textViewError.visibility = View.VISIBLE
-//                            listViewWeather.visibility = View.GONE
-//                        }
-//                )
+            Communicator.service(this)!!.getWeather("b6add83687f743fb94a150227171510", coords, "yes", 24)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            { data ->
+                                Log.d("test_test", "data: " + data.weather!![0].date)
+                                WeatherStorage.set(data.weather!!)
+                                setChartData()
+                            },
+                            { error ->
+                                Log.d("test_test", "Failed to get weather data, error: " + error.toString())
+                                showChartError()
+                              //  lineChart!!.invalidate()
+                            }
+                    )
+        }
 
     }
 
 
-    fun rebuildChart() {
-        if (chartView != null) {
-            val entries = ArrayList<Entry>()
-            val r = Random()
-            for (i in 0..10) {
-                entries.add(Entry(i.toFloat(), (r.nextInt(10 - 5) + 5).toFloat()))
+    fun setupChart() {
+
+        lineChart = chartView
+        lineChart!!.setNoDataText("")
+        lineChart!!.xAxis.position = XAxis.XAxisPosition.BOTTOM;
+        //lineChart!!.xAxis.granularity = 1f
+        lineChart!!.xAxis.valueFormatter = object: IAxisValueFormatter {
+            private val format = SimpleDateFormat("HH:mm")
+
+            override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+                var millis = TimeUnit.HOURS.toMillis(value.toLong())
+                return format.format( Date(millis))
             }
 
-            val dataSet = LineDataSet(entries, "Label") // add entries to dataset
-            dataSet.setColor(Color.BLUE)
-            dataSet.setValueTextColor(Color.BLACK)
-            dataSet.fillColor = Color.RED
-            dataSet.setDrawFilled(true)
-            dataSet.setDrawValues(false)
-
-            dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-
-            val lineChart: LineChart = chartView
-
-            val lineData = LineData(dataSet);
-            lineChart.setData(lineData);
-            lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM;
-            lineChart.xAxis.valueFormatter = object: IAxisValueFormatter {
-                private val format = SimpleDateFormat("HH:mm")
-
-                override fun getFormattedValue(value: Float, axis: AxisBase?): String {
-                     var millis = TimeUnit.HOURS.toMillis(value.toLong())
-                     return format.format( Date(millis))
-                }
-
-            }
-            lineChart.axisRight.isEnabled = false
-            lineChart.invalidate()
         }
+        lineChart!!.axisRight.isEnabled = false
+        lineChart!!.axisLeft.valueFormatter = object: IAxisValueFormatter {
+            override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+                return String.format("%.1f", value) + " ft"
+            }
+        }
+
+        lineChart!!.description.isEnabled = false;
+        lineChart!!.legend.isEnabled = false
+        //lineChart!!.invalidate()
+
+    }
+
+
+    fun showChartError() {
+        lineChart!!.setNoDataText("Failed to get tides data")
+        lineChart!!.setNoDataTextColor(ContextCompat.getColor(this, R.color.red))
+        val p = lineChart!!.getPaint(Chart.PAINT_INFO);
+        p.textSize = 50f
+        lineChart!!.invalidate()
+    }
+
+
+    fun setChartData() {
+        if (lineChart == null) {
+            return
+        }
+
+        val entries = ArrayList<Entry>()
+        val r = Random()
+        for (i in 0..9) {
+            entries.add(Entry(i.toFloat(), (r.nextInt(10) - 5).toFloat()))
+       }
+
+        val dataSet = LineDataSet(entries, "Label") // add entries to dataset
+        dataSet.setColor(ContextCompat.getColor(this, R.color.tides_chart_line_color))
+        dataSet.setValueTextColor(Color.BLACK)
+        dataSet.fillColor = Color.RED
+        dataSet.lineWidth = 2.0f
+        dataSet.setDrawFilled(true)
+        dataSet.setDrawValues(false)
+        dataSet.fillColor = ContextCompat.getColor(this, R.color.tides_chart_fill_color)
+
+
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER)
+        val lineData = LineData(dataSet)
+        lineChart!!.setData(lineData)
+        lineChart!!.invalidate()
     }
 
 
