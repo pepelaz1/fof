a2abbec285c247e20ad6c93c7b2a950857869a40
Vladimir Baldetskiy <pepelaz101@gmail.com>
2018-06-05 18:37:07 +0700

    !

--- fof/app/src/main/java/ru/pepelaz/fof/activities/CompassActivity.kt
+++ fof/app/src/main/java/ru/pepelaz/fof/activities/CompassActivity.kt
@@ -1,6 +1,8 @@
 package ru.pepelaz.fof.activities
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -10,19 +12,28 @@ import android.view.View
 import ru.pepelaz.fof.R
 import android.widget.TextView
 import android.hardware.SensorManager
+import android.support.v4.app.ActivityCompat
 import android.widget.ImageView
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import kotlinx.android.synthetic.main.activity_compass.*
 import ru.pepelaz.fof.R.id.imageViewCompass
+import ru.pepelaz.fof.helpers.CurrentCoords
 
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 
-
-
-class CompassActivity : AppCompatActivity(), SensorEventListener {
+class CompassActivity : AppCompatActivity(), SensorEventListener,  OnMapReadyCallback {
 
 
     private var currentDegree = 0f
+    private var map: GoogleMap? = null
+
     private val sensorManager: SensorManager by lazy {
         getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
@@ -31,17 +42,20 @@ class CompassActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compass)
 
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onResume() {
         super.onResume()
         if (sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
                 SensorManager.SENSOR_DELAY_NORMAL) == false) {
-            textViewError.visibility = View.VISIBLE
-            imageViewCompass.visibility = View.GONE
+            layoutError.visibility = View.VISIBLE
+            layoutCompass.visibility = View.GONE
         } else {
-            textViewError.visibility = View.GONE
-            imageViewCompass.visibility = View.VISIBLE
+            layoutError.visibility = View.GONE
+            layoutCompass.visibility = View.VISIBLE
         }
     }
 
@@ -54,7 +68,8 @@ class CompassActivity : AppCompatActivity(), SensorEventListener {
         // get the angle around the z-axis rotated
         val degree = Math.round(event!!.values[0])
 
-        textViewHeading.setText("Heading: " + degree.toString() + " degrees")
+        textViewDegree.setText( degree.toString() + 0x00B0.toChar())
+        textViewTitle.text = evaluateTitleFromDegree(degree)
 
         // create a rotation animation (reverse turn degree degrees)
         val ra = RotateAnimation(
@@ -75,8 +90,59 @@ class CompassActivity : AppCompatActivity(), SensorEventListener {
         currentDegree = -degree.toFloat()
     }
 
+    fun evaluateTitleFromDegree(degree: Int) : String {
+
+        var result = ""
+        if (degree >= 0 && degree < 45) {
+            result = "North"
+        } else if (degree >= 45 && degree < 90) {
+            result = "North-East"
+        } else if (degree >= 90 && degree < 135) {
+            result = "East"
+        } else if (degree >= 135 && degree < 180) {
+            result = "South-East"
+        } else if (degree >= 180 && degree < 225) {
+            result = "South"
+        } else if (degree >= 225 && degree < 270) {
+            result = "South-West"
+        } else if (degree >= 270 && degree < 315) {
+            result = "West"
+        } else if (degree >= 315 && degree < 360) {
+            result = "North-West"
+        }
+        return result
+    }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Log.d("test_test","on map ready")
+        map = googleMap
+
+        map!!.mapType = GoogleMap.MAP_TYPE_TERRAIN
+        map!!.uiSettings.setZoomControlsEnabled(true)
+        map!!.uiSettings.setCompassEnabled(true)
+        map!!.uiSettings.setMyLocationButtonEnabled(true)
+        map!!.uiSettings.setAllGesturesEnabled(true)
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        map!!.setMyLocationEnabled(true);
+
+        map!!.setOnMapClickListener {
+            //            Log.d("test_test","on map click")
+//            if (marker != null)
+//                marker!!.remove()
+//
+//            marker = map!!.addMarker(MarkerOptions().position(it).title("Me"))
+//            map!!.moveCamera(CameraUpdateFactory.newLatLngZoom(it, 13.0f))
+        }
+
+        //latitude = CurrentCoords.latitude
+        //longitude = CurrentCoords.longitude
+        //updateMapPosition()
     }
 }
