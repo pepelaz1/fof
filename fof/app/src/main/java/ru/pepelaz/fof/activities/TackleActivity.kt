0a1265337c15a20b1508fe65eb5785dc2e42a1a8
Vladimir Baldetskiy <pepelaz101@gmail.com>
2018-07-02 17:48:54 +0700

    1

--- fof/app/src/main/java/ru/pepelaz/fof/activities/TackleActivity.kt
+++ fof/app/src/main/java/ru/pepelaz/fof/activities/TackleActivity.kt
@@ -3,16 +3,18 @@ package ru.pepelaz.fof.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_tackle.*
 import ru.pepelaz.fof.R
+import ru.pepelaz.fof.fragments.PageFragment
 
 class TackleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tackle)
-    }
 
-    fun buttonCloseClick(v: View) {
-        finish()
+        (fragmentPage as PageFragment).offlineUrl = "file:///android_asset/pages/tackle/tackle.htm"
+        (fragmentPage as PageFragment).onlineUrl = "http://bit.ly/2KNOMu6"
+        (fragmentPage as PageFragment).loadOfflineContent()
     }
 }
